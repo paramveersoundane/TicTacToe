936c8dd02f152ef6bc7cf05dc0732865742b5259
paramveersoundane <paramveersoundane@gmail.com>
2020-09-07 09:49:11 +0530

    basic app wih all functionality

---
+++ src/app/app.model.ts
@@ -0,0 +1,11 @@
+export class TicTocToe{
+    flagvisited11:string;
+    flagvisited12:string;
+    flagvisited13:string;
+    flagvisited21:string;
+    flagvisited22:string;
+    flagvisited23:string;
+    flagvisited31:string;
+    flagvisited32:string;
+    flagvisited33:string;
+}
